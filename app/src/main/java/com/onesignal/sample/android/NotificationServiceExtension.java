4243fd21008d1069096bd4846895f943cad0cbb4
William Shepherd <williamrshepherd@gmail.com>
2024-04-10 15:13:01 -0500

    Add high-level plan to cancel a live update

--- app/src/main/java/com/onesignal/sample/android/NotificationServiceExtension.java
+++ app/src/main/java/com/onesignal/sample/android/NotificationServiceExtension.java
@@ -5,6 +5,8 @@ import com.onesignal.notifications.IDisplayableMutableNotification;
 import com.onesignal.notifications.INotificationReceivedEvent;
 import com.onesignal.notifications.INotificationServiceExtension;
 
+import org.json.JSONException;
+
 public class NotificationServiceExtension implements INotificationServiceExtension {
     @Override
     public void onNotificationReceived(INotificationReceivedEvent event) {
@@ -19,9 +21,26 @@ public class NotificationServiceExtension implements INotificationServiceExtensi
         // this is an example of how to modify the notification by changing the background color to blue
         notification.setExtender(builder -> builder.setColor(0xFF0000FF));
         notification.setExtender(builder -> builder.setAutoCancel(false));
+        try {
+            // We could check some property on the notification that we set to determine
+            // whether the notification should be cancelled.
+            // Ex uses "OSAction": "stop"
+            // We could potentially use `dismissal_date` to determine to cancel
+            if (event.getNotification().getAdditionalData().get("OSAction") == "stop") {
+                // We can get the notification ID from the event itself
+                int notificationId = Integer.parseInt(event.getNotification().getNotificationId());
 
-        //If you need to perform an async action or stop the payload from being shown automatically,
-        //use event.preventDefault(). Using event.notification.display() will show this message again.
+                // Does our SDK provide a hook into this?
+                // My assumption is the SDK has a manager to manage all incoming OneSignal notifications
+
+                // NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                // Assuming we can cancel
+                //notificationManager.cancel(notificationId);
+            }
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
 
