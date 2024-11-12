7d247d75026ce7453ce1ee9c3a89bd815a137cc0
William Shepherd <williamrshepherd@gmail.com>
2024-11-12 11:43:31 -0600

    Authenticate before logging into OneSignal

--- app/src/main/java/com/onesignal/sample/android/MainActivity.kt
+++ app/src/main/java/com/onesignal/sample/android/MainActivity.kt
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.onesignal.OneSignal
 import android.widget.Button
+import okhttp3.*
+import okhttp3.RequestBody.Companion.toRequestBody
 
 class MainActivity : AppCompatActivity() {
+    private val client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.login).setOnClickListener {
-            OneSignal.login("will")
+            run()
+
         }
 
         findViewById<Button>(R.id.logout).setOnClickListener {
@@ -33,5 +38,34 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.present_iam).setOnClickListener {
             OneSignal.InAppMessages.addTrigger("show_sample_iam", "1")
         }
+    }
+
+
+    fun run() {
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url("http://localhost:3000/auth")
+            .post("".toRequestBody())
+            .build()
+
+        println("Logging in")
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: java.io.IOException) {
+                // Handle failure
+                print("An error occurred")
+                print(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                // Handle success
+                val result = response.body?.string() ?: ""
+                // Process the response data
+
+                OneSignal.login("will")
+                println("Auth server response")
+                println(result)
+            }
+        })
     }
 }
