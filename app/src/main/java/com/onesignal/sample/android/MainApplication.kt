94390933572322325794df28ddafddb791caeb72
William Shepherd <williamrshepherd@gmail.com>
2024-11-12 11:43:09 -0600

    Turn off verbose OS logging

--- app/src/main/java/com/onesignal/sample/android/MainApplication.kt
+++ app/src/main/java/com/onesignal/sample/android/MainApplication.kt
@@ -8,7 +8,7 @@ const val ONESIGNAL_APP_ID = "305964ee-f979-4054-83e7-4c2096f2b0b4"
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        OneSignal.Debug.logLevel = LogLevel.VERBOSE
+//        OneSignal.Debug.logLevel = LogLevel.VERBOSE
         OneSignal.initWithContext(this, ONESIGNAL_APP_ID)
     }
 }
